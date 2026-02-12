b1d9454e1aa63a87ab6fc259c4346b74db0790ce
Zero-rest <46242415+Zero-rest@users.noreply.github.com>
2026-02-12 18:38:18 +0330

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,8 @@
+StudentDispatch (Android / Kotlin / Jetpack Compose / Room)
+
+Run:
+- Open this folder in Android Studio (File > Open)
+- Sync Gradle
+- Run on emulator/device
+
+Admin password: 123456
---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        // Mirrors (خیلی کمک می‌کنه وقتی Google/Maven کند یا بسته است)
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "StudentDispatch"
+include(":app")
